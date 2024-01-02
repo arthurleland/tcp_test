353b7b74721bfe58dadb84ae3caff4cb2e1d6e4b
arthurleland <arthurleland@gmail.com>
2024-01-02 13:58:22 -0800

    u

--- echo_client.py
+++ echo_client.py
@@ -8,6 +8,7 @@ def main():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
         s.connect((HOST, PORT))
         print(f"connected via {s.getsockname()}")
 
