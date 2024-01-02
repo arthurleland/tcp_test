37fbe674f1dc7347c05d8a0237356a5f001dab08
arthurleland <arthurleland@gmail.com>
2024-01-02 13:42:17 -0800

    u

--- echo_client.py
+++ echo_client.py
@@ -3,10 +3,11 @@ import sys
 
 
 def main():
-    HOST = "127.0.0.1"  # The server's hostname or IP address
+    HOST = "192.168.0.124"  # The server's hostname or IP address
     PORT = 10000  # The port used by the server
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.connect((HOST, PORT))
         print(f"connected via {s.getsockname()}")
 
--- echo_server.py
+++ echo_server.py
@@ -2,11 +2,12 @@ import socket
 
 
 def main():
-    HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+    HOST = ""  # Standard loopback interface address (localhost)
     PORT = 10000  # Port to listen on (non-privileged ports are > 1023)
 
     while True:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             s.bind((HOST, PORT))
             s.listen()
             conn, addr = s.accept()
