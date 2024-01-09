422ba5a2ab15bba0b0db9bd90bf5754339914c77
arthurleland <arthurleland@gmail.com>
2024-01-08 17:04:27 -0800

    U

--- connect.py
+++ connect.py
@@ -3,14 +3,14 @@ import sys
 
 
 def main():
-    BIND_HOST = "10.1.10.111"  # this specifies the network interface to use
-    HOST = "127.0.0.1"  # The server's hostname or IP address
-    # HOST = "67.169.190.38"  # The server's hostname or IP address
+    CLIENT = "10.1.10.111"  # this specifies the network interface to use
+    SERVER = "127.0.0.1"  # The server's hostname or IP address
+    # SERVER = "67.169.190.38"  # The server's hostname or IP address
     PORT = 10000  # The port used by the server
 
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc.bind((BIND_HOST, 0))
-    soc.connect((HOST, PORT))
+    soc.bind((CLIENT, 0))
+    soc.connect((SERVER, PORT))
     soc.close()
 
     print("connect all done")
--- listen.py
+++ listen.py
@@ -3,12 +3,12 @@ import sys
 
 
 def main():
-    HOST = "10.1.10.120"  # Standard loopback interface address (localhost)
+    SERVER = "10.1.10.120"  # Standard loopback interface address (localhost)
     PORT = 10000  # Port to listen on (non-privileged ports are > 1023)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
+    s.bind((SERVER, PORT))
     s.listen()
     conn, addr = s.accept()
     conn.close()
@@ -17,7 +17,7 @@ def main():
     print("listen all done")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
+        s.bind((SERVER, PORT))
         s.listen()
         try:
             conn, addr = s.accept()
