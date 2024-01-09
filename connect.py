6f6a7d2f1ee5d9e478c8e2666f18eb4007d6669e
arthurleland <arthurleland@gmail.com>
2024-01-09 12:16:52 -0800

    u

--- connect.py
+++ connect.py
@@ -3,8 +3,8 @@ import sys
 
 
 def main():
-    CLIENT = "10.1.10.111"  # this specifies the network interface to use
-    SERVER = "127.0.0.1"  # The server's hostname or IP address
+    CLIENT = "192.168.12.101"  # this specifies the network interface to use
+    SERVER = "192.168.12.100"  # The server's hostname or IP address
     # SERVER = "67.169.190.38"  # The server's hostname or IP address
     PORT = 10000  # The port used by the server
 
@@ -13,10 +13,7 @@ def main():
     soc.connect((SERVER, PORT))
     soc.close()
 
-    print("connect all done")
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-        soc.connect((HOST, PORT))
+    print("client all done")
 
 
 if __name__ == "__main__":
--- listen.py
+++ listen.py
@@ -3,7 +3,7 @@ import sys
 
 
 def main():
-    SERVER = "10.1.10.120"  # Standard loopback interface address (localhost)
+    SERVER = "192.168.12.100"  # Standard loopback interface address (localhost)
     PORT = 10000  # Port to listen on (non-privileged ports are > 1023)
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -13,18 +13,7 @@ def main():
     conn, addr = s.accept()
     conn.close()
     s.close()
-
-    print("listen all done")
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((SERVER, PORT))
-        s.listen()
-        try:
-            conn, addr = s.accept()
-        finally:
-            conn.close()
-
-        print("all done")
+    print("server all done")
 
 
 if __name__ == "__main__":
